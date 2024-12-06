a9c4fc22af6bbcac1f828538165e3fe47ccc5a2b
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-12-06 07:22:30 -0500

    add table of content

--- src/main/java/me/hossain/ebrahim/variabletypes/StaticVariables/StaticVariables.java
+++ src/main/java/me/hossain/ebrahim/variabletypes/StaticVariables/StaticVariables.java
@@ -2,14 +2,14 @@ package me.hossain.ebrahim.variabletypes.StaticVariables;
 
 public class StaticVariables {
 
-    static String name_static;
+    static String name;
     static long number;
 
     public static void main(String[] args) {
-        name_static = "Ebrahim Hossain";
+        name = "Ebrahim Hossain";
         number = 1234;
         System.out.println("Print Static Variables");
-        System.out.println("Name : " + name_static);
+        System.out.println("Name : " + name);
         System.out.println("Number : " + number);
     }
 }
